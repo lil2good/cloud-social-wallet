710466a8db3e7c4c94313118b9d7e2501f40508f
shoaib-blocksdev <shoaib@blocksdev.com>
2025-01-28 12:26:14 +0500

    domain cookie added

--- src/hooks/useConnectWallet.tsx
+++ src/hooks/useConnectWallet.tsx
@@ -44,7 +44,7 @@ const useConnectWallet = ({config}:{config: Config}) => {
         transports: ["websocket"],
         withCredentials: true
     });
-    console.log("====config====",config)
+
     useEffect(() => {
         socket.on("getTokenRes", (msg) => {
             if(msg.success && socket.id === msg.id) {
@@ -88,8 +88,8 @@ const useConnectWallet = ({config}:{config: Config}) => {
         const handler = (event:any) => {
             if (event.data.address) {
                 setAddress(event.data.address)
-                Cookie.set("cookie", event.data.cookie);
-                Cookie.set("address", event.data.address);
+                Cookie.set("cookie", event.data.cookie,{domain: ".loop.fans"});
+                Cookie.set("address", event.data.address,{domain: ".loop.fans"});
                 getToken();
             }
         };
